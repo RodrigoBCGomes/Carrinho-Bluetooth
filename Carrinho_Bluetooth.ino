45adce3c928371c379c52e38d777aec4712e7d65
RodrigoBCGomes <rodrigosgtau@gmail.com>
2023-04-26 16:29:23 -0300

    Criando os motores da ponte H

--- Carrinho_Bluetooth.ino
+++ Carrinho_Bluetooth.ino
@@ -1,3 +1,17 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial bt(0,1);
+
+// Lado esquerdo
+int ENA = 10;
+int IN1 = 9;
+int IN2 = 8;
+
+// Lado direito
+int ENB = 5;
+int IN3 = 7;
+int IN4 = 6;
+
 void setup() {
 
 }
