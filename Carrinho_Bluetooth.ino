51e76acc195eca19d8f693ea782ef3b68b85931d
RodrigoBCGomes <rodrigosgtau@gmail.com>
2023-04-26 16:33:04 -0300

    Setando saídas do arduino e colocando velocidade máxima

--- Carrinho_Bluetooth.ino
+++ Carrinho_Bluetooth.ino
@@ -12,8 +12,18 @@ int ENB = 5;
 int IN3 = 7;
 int IN4 = 6;
 
-void setup() {
+char t;
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  analogWrite(ENA,255);
+  analogWrite(ENB,255);
 }
 
 void loop() {
